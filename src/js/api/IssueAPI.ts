27f6edebefccce876dd5e151848ac6f9d40aceda
diffshare <diffshare@gmail.com>
2015-10-03 17:34:16 +0900

    refactoring 整形はservices側で

--- src/js/api/IssueAPI.ts
+++ src/js/api/IssueAPI.ts
@@ -6,6 +6,23 @@ export default class IssueAPI {
 
     fetchIssues():ng.IPromise<any> {
         let url = "/assets/issues.json";
-        return this.$http.get(url);
+        return this.$http.get(url)
+            .then((result:any)=> {
+                // 整形は利用するエンドポイントごとに異なるので、APIに押し込む
+                return result.data.issues.map((issue:any) => {
+                    for (var c of issue.custom_fields) {
+                        if (c.name == "場所")
+                            [issue.latitude, issue.longitude] = c.value.split(",");
+                    }
+                    return {
+                        id: issue.id,
+                        latitude: issue.latitude,
+                        longitude: issue.longitude,
+                        title: issue.subject,
+                        description: issue.description,
+                        //author: issue.author.name
+                    };
+                });
+            });
     }
 }
--- src/js/directives/RootDirective.ts
+++ src/js/directives/RootDirective.ts
@@ -21,20 +21,7 @@ class RootController {
     }
 
     async updateIssues() {
-        var result = await this.IssueAPI.fetchIssues();
-        this.issues = result.data.issues.map((issue:any) => {
-            for (var c of issue.custom_fields) {
-                if (c.name == "場所")
-                    [issue.latitude, issue.longitude] = c.value.split(",");
-            }
-            return {
-                id: issue.id,
-                latitude: issue.latitude,
-                longitude: issue.longitude,
-                title: issue.subject,
-                description: issue.description,
-                //author: issue.author.name
-            };
-        });
+        // XXX tryによるエラーハンドリング
+        this.issues = await this.IssueAPI.fetchIssues();
     }
 }

55d20fc957c0c839fb8663ca1e5fc62a28df7364
diffshare <diffshare@gmail.com>
2016-01-29 21:48:49 +0900

    サービスの実装

--- src/app/home.ts
+++ src/app/home.ts
@@ -1,7 +1,20 @@
 import {Component} from "angular2/core";
+import {ListService} from "./services/list_service";
 
 @Component({
+    providers: [ListService],
     template: require("./home.slim")
 })
 export class Home {
+
+    list: Array<any>;
+
+    constructor(private listService:ListService) {}
+
+    ngOnInit() {
+        this.listService.fetch()
+            .subscribe((result:any) => {
+                this.list = result;
+            });
+    }
 }

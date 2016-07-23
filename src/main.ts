024a15d0e9b7767c04fdf7acdd741f02e7fc7fe9
diffshare <diffshare@gmail.com>
2016-07-23 23:23:29 +0900

    s3にアップロードするための準備

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import {bootstrap} from '@angular/platform-browser-dynamic'
 import {App} from './app/app';
 import {HTTP_PROVIDERS} from "@angular/http";
 import {appRouterProviders} from "./app/app.routes";
+import {LocationStrategy, HashLocationStrategy} from "@angular/common";
+import {provide} from "@angular/core";
 
 //noinspection TypeScriptValidateTypes
 bootstrap(App, [
     ...HTTP_PROVIDERS,
-    appRouterProviders
+    appRouterProviders,
+    provide(LocationStrategy, {useClass: HashLocationStrategy})
 ]).catch(err => console.error(err));
